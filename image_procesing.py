5ab5d9e3d49964c4c4c491e798135184ff8a8b47
richardtomik <149070450+richardtomik@users.noreply.github.com>
2023-10-26 16:16:18 +0200

    initial commit

---
+++ image_procesing.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+
+# Load the image
+image = cv2.imread('garden.png')
+
+if image is None:
+    print("Image not found or could not be loaded.")
+else:
+    image = cv2.blur(image, (40,40))
+    #image = cv2.medianBlur(image, 10)
+    # Convert the image to HSV color space
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define the lower and upper HSV ranges for green and yellow
+    lower_green = np.array([35, 50, 50])
+    upper_green = np.array([85, 255, 255])
+
+    lower_yellow = np.array([20, 100, 100])
+    upper_yellow = np.array([40, 255, 255])
+
+    # Create masks to isolate all shades of green and yellow
+    green_mask = cv2.inRange(hsv_image, lower_green, upper_green)
+    yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+
+    # Combine the masks to detect all shades of green and yellow
+    combined_mask = cv2.bitwise_or(green_mask, yellow_mask)
+
+    # Invert the mask so that all shades of green and yellow become white (1) and the rest becomes black (0)
+    kernel = np.ones((7, 7), np.uint8)
+    combined_mask2 = cv2.erode(combined_mask, kernel, iterations=0)
+    
+    
+    inverted_mask = cv2.bitwise_not(combined_mask2)
+
+    # Erode the mask to ignore single pixels
+    
+     
+    # Resize the inverted mask to the desired grid size (300x200)
+    grid_size = (300, 200)
+    resized_mask = cv2.resize(inverted_mask, grid_size)
+
+    # Save the grid as an image
+    cv2.imwrite('grid_image.png', resized_mask)
+
+    print("Grid image has been saved to 'grid_image.png'.")
