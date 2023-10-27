924b37e73150c184c75a91c34711bb9dd4228b29
petrpejsa <petrpejsa@gmail.com>
2023-10-27 08:16:12 +0200

    BFS neotestovany

--- BFS.py
+++ BFS.py
@@ -1,23 +1,11 @@
 import numpy as np
-import math
 
 
-def manhattan_distance(point1, point2):
-    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-
-
-class AStar:
+class BFS:
     def __init__(self, grid):
         self.grid = grid
 
-    def find_path(self, start, end):
-        def get_closest_open():
-            closest = open_nodes[0]
-            for open_node in open_nodes[1:]:
-                if cost_f[open_node] < cost_f[closest]:
-                    closest = open_node
-            return closest
-
+    def find_path(self, start: tuple, target: int):
         def get_neighbours(node):
             neighbours = []
             if node[0] > 0:
@@ -30,7 +18,7 @@ class AStar:
                 neighbours.append((node[0], node[1] + 1))
             return neighbours
 
-        def get_result_path():
+        def get_result_path(end):
             result = []
             tmp = end
             while tmp != start:
@@ -38,38 +26,35 @@ class AStar:
                 tmp = came_from[tmp]
             return list(reversed(result))
 
-
         closed_nodes = []
         open_nodes = [start]
-        cost_g = np.copy(self.grid) * self.grid.size
-        cost_f = np.copy(self.grid) * self.grid.size
-        cost_g[start] = 0
-        cost_f[start] = manhattan_distance(start, end)
-        # cost_f[start] = math.dist(start, end)
+        cost = np.copy(self.grid) * self.grid.size
+        cost[start] = 0
         came_from = {}
 
         while open_nodes:
-            current = get_closest_open()
-            if current == end:
-                return get_result_path()
-            open_nodes.remove(current)
+            current = open_nodes.pop(0)
+            if self.grid[current] == target:
+                return get_result_path(current)
             closed_nodes.append(current)
             for neighbour in get_neighbours(current):
                 if neighbour in closed_nodes:
                     continue
-                neighbour_cost = cost_g[current] + 1
+                if abs(self.grid[neighbour]) != target and self.grid[neighbour] != 0:
+                    continue  # move only on dedicated cells or non-mowed cells
+                neighbour_cost = cost[current] + 1
                 if neighbour not in open_nodes:
                     open_nodes.append(neighbour)
-                elif bool(neighbour_cost >= cost_g[neighbour] != 0):
+                elif bool(neighbour_cost >= cost[neighbour] != 0):
                     continue
                 came_from[neighbour] = current
-                cost_g[neighbour] = neighbour_cost
-                cost_f[neighbour] = manhattan_distance(neighbour, end)
-                # cost_f[neighbour] = math.dist(neighbour, end)
+                cost[neighbour] = neighbour_cost
 
         raise FileNotFoundError("Cesta nenalezena :D")
 
 
 if __name__ == '__main__':
-    a_star = AStar(np.load('grid_image.npy'))
-    print(a_star.find_path((0, 0), (199, 299)))
+    image = np.load('grid_image.npy')
+    image[(199, 299)] = 2
+    a_star = BFS(image)
+    print(a_star.find_path((0, 0), 2))
