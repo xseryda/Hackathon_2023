b6f762cecdd41f7a07f3e9e96c38e74f2710fe07
David Šerý <serezady@centrum.cz>
2023-10-27 10:29:22 +0200

    Multiagent mowing now expects grid separated for individual agents.

--- coverage_planner.py
+++ coverage_planner.py
@@ -1,22 +1,33 @@
-import os
-
 import matplotlib.pyplot as plt
 import numpy as np
+
+from BFS import BFS
 
 
 DRAW_STEP = 100
 
 
-class GRASS_STATE:
+class GrassState:
     OBSTACLE = 0
 
 
 class Agent:
     def __init__(self, agent_id, grid, i, j):
+        self._bfs = BFS(grid)
         self._id = agent_id
         self._grid = grid
+        self._path = []
         self._i = i
         self._j = j
+        self._unmowed = 0
+
+    @property
+    def active(self):
+        return self._unmowed > 0
+
+    @property
+    def id(self):
+        return self._id
 
     @property
     def i(self):
@@ -26,16 +37,33 @@ class Agent:
     def j(self):
         return self._j
 
+    @property
+    def unmowed(self):
+        return self._unmowed
+
+    @unmowed.setter
+    def unmowed(self, value):
+        self._unmowed = value
+
+    def extend_path(self, i, j):
+        self._path.append((i, j))
+        self._unmowed -= 1
+
     def get_next_position(self, i, j):
-        if self._grid[i, j - 1] == 0:  # TODO use ID here
+        if self._grid[i, j - 1] == self.id:
             return i, j - 1
-        if self._grid[i + 1, j] == 0:
+        if self._grid[i + 1, j] == self.id:
             return i + 1, j
-        if self._grid[i - 1, j] == 0:
+        if self._grid[i - 1, j] == self.id:
             return i - 1, j
-        if self._grid[i, j + 1] == 0:
+        if self._grid[i, j + 1] == self.id:
             return i, j + 1
         return None
+
+    def move_to_unmowed(self):
+        path = self._bfs.find_path((self.i, self.j), self.id)
+        self._path.extend(path)
+        self.set_position(*path[-1])
 
     def set_position(self, i, j):
         self._i = i
@@ -52,36 +80,39 @@ class CoveragePlanner:
 
     def __init__(self, grid):
         self._agents = []
-        self._grid = np.pad(grid, 1, constant_values=1)
-        unique, counts = np.unique(grid, return_counts=True)
-        self._unmowed = counts[0]
+        self._grid = np.pad(grid, 1, constant_values=GrassState.OBSTACLE)
+        self._unmowed = None
         self._path = []
 
-    def _find_closest(self, i, j):
-        r, c = np.nonzero(self._grid == 0)
-        min_idx = (abs(r - i) + abs(c - j)).argmin()
-        return r[min_idx], c[min_idx]
-
     def add_agent(self, i, j):
-        i, j = self._find_closest(i, j)
         agent_id = len(self._agents) + 1
-        self._agents.append(Agent(agent_id, self._grid, i, j))
+        self._agents.append(Agent(agent_id, self._grid, i+1, j+1))
 
     def start(self):
-        plt.cla()
+        unique, counts = np.unique(self._grid, return_counts=True)
+        self._unmowed = 0
+        for i, agent in enumerate(self._agents, start=1):
+            agent.unmowed = counts[i]
+            self._unmowed += agent.unmowed
         img = plt.imshow(self._grid[1:-1, 1:-1], vmin=-len(self._agents),
-                         vmax=len(self._agents))
+                         vmax=len(self._agents) + 1)
         step = 0
         while True:
             for agent in self._agents:
+                if self._unmowed == 0:
+                    print('Finished')
+                    return
                 i, j = agent.i, agent.j
-                if self._grid[i, j] < 1:
+                if self._grid[i, j] == agent.id:
                     self._unmowed -= 1
-                    self._grid[i, j] = 2
-                    self._path.append((i, j))
+                    self._grid[i, j] = -agent.id
+                    agent.extend_path(i, j)
+                    if not agent.active:
+                        continue
                     position = agent.get_next_position(i, j)
                     if position is None:
-                        print('Stop')
+                        print(f'Stop {agent.id=}')
+                        agent.move_to_unmowed()
                         continue
                     step += 1
                     i, j = position
@@ -89,19 +120,17 @@ class CoveragePlanner:
                     if step % DRAW_STEP == 0:
                         img.set_data(self._grid[1:-1, 1:-1])
                         plt.pause(0.01)
-                if self._unmowed == 0:
-                    print('Finished')
-                    return
-                agent.set_position(*self._find_closest(i, j))
 
 
 def main():
-    grid = np.load('grid_image.npy')
+    grid = np.load('grid_color.npy')
     #grid = plt.imread(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'map', 'test_3.png'))
     #grid = 1 - grid  # revert pixel values
     planner = CoveragePlanner(grid)
-    planner.add_agent(1, 1)
-    planner.add_agent(grid.shape[0], 1)
+    planner.add_agent(0, 0)
+    planner.add_agent(0, 299)
+    planner.add_agent(199, 0)
+    planner.add_agent(199, 299)
 
     planner.start()
 
